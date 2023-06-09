8100b380cbdd448b3086fdf51444301c03aed33e
Snehasrg <sghongade642@gmail.com>
2023-06-09 16:39:47 +0530

    [sneha]Added Program for Employee Wage Problem

---
+++ CompanyEmpWage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public class CompanyEmpWage
+    {
+        public string company;
+        public int Wage_Per_Hour;
+        public int Max_Days_IN_Month;
+        public int Max_HrsInMonth;
+        public int sallery;
+        public CompanyEmpWage(string company, int Wage_Per_Hour, int Max_Days_IN_Month, int Max_HrsInMonth)
+        {
+            this.company = company;
+            this.Wage_Per_Hour = Wage_Per_Hour;
+            this.Max_Days_IN_Month = Max_Days_IN_Month;
+            this.Max_HrsInMonth = Max_HrsInMonth;
+
+        }
+        public void setEmpWage(int sallery)
+        {
+            this.sallery = sallery;
+        }
+        public string Return()
+        {
+            return ("\n Total employee wage of " + this.company + " is :-  " + this.sallery);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,11 +4,14 @@
     {
         public static void Main(String[] args)
         {
+
             EmployeeWage obj = new EmployeeWage();
 
-            obj.Employee("Google", 20, 15, 80);
-            Console.WriteLine("------------------------------------------------------");
-            obj.Employee("Microsoft", 25, 10, 60);
+            obj.addCompanies("Google", 20, 15, 90);
+
+            obj.addCompanies("Microsoft", 45, 23, 150);
+
+            obj.EmpComputation();
         }
     }
 }
