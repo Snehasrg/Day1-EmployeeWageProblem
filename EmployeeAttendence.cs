caccf84f575f254c8db3146d45acde4baac3496e
Snehasrg <sghongade642@gmail.com>
2023-06-09 15:26:53 +0530

    [sneha]Added program for Employee Wage Problem

---
+++ EmployeeAttendence.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public class EmployeeAttendence
+    {
+        public static void CheckAttendence()
+        {
+            int isFullTime = 1;
+            Random r = new Random();
+            int empcheck = r.Next(2);
+            if(empcheck==1)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
